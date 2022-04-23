da05305622a8e5b9cbbdefa8b1812351fd16b50d
venskoA <venskoandrey@gmail.com>
2022-04-23 19:37:06 +0300

    LessonFive. Task one. Pattern flyweight

--- courseTwoOne/Extensions/ExtensionUIFont.swift
+++ courseTwoOne/Extensions/ExtensionUIFont.swift
@@ -5,4 +5,10 @@
 //  Created by Andrey Vensko on 13.04.22.
 //
 
-import Foundation
+import UIKit
+
+extension UIFont {
+    static func fontLabel() -> UIFont? {
+        return UIFont(name: "Avenir Next", size: 15)
+    }
+}
--- courseTwoOne/GlobalExtension/StackView+.swift
+++ courseTwoOne/GlobalExtension/StackView+.swift
@@ -5,4 +5,14 @@
 //  Created by Andrey Vensko on 20.02.22.
 //
 
-import Foundation
+import UIKit
+
+extension UIStackView {
+    convenience init(subview: [UIView], axis: NSLayoutConstraint.Axis, spacing: CGFloat, distribution: UIStackView.Distribution) {
+        self.init(arrangedSubviews: subview)
+        self.axis = axis
+        self.spacing = spacing
+        self.distribution = distribution
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
